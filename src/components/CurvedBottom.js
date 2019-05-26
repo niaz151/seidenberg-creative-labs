6d176b6cc3a146dc5e067b073660b0b96fae6c3b
Niaz Ahmed <niaz151@gmail.com>
2019-05-25 22:32:25 -0400

    Crated Simple Project Structure

---
+++ src/components/CurvedBottom.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CurvedBottom extends React.Component{
+
+  render(){
+    return(
+      <div>
+        Curved Bottom
+      </div>
+    )
+  }
+}
+
+export default CurvedBottom;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component{
+
+  render(){
+    return(
+      <div className="border border-primary d-flex w-100 header">
+        Header
+      </div>
+    )
+  }
+}
+
+export default Header;
