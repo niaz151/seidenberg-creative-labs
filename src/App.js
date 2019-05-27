ec562baf7326b5bcbd2d9d0f40147a2106130d2d
NiazAhmed151 <niaz151@gmail.com>
2019-05-27 16:16:02 -0400

    Added Header Categories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React,{Fragment} from 'react';
+import Header from './components/Header'
+
 
 function App() {
   return (
-    <div>
-      hi
+    <div className="d-flex" style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+      <Header/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,29 @@ class Header extends React.Component{
 
   render(){
     return(
-      <div className="border border-primary d-flex w-100 header">
-        Header
-      </div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light container">
+        <a class="navbar-brand" href="#"> SCL </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Team <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Skills</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Projects</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     )
   }
 }
