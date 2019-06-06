ea9a6a0b57de7f78447e2992e8779dd930d78212
NiazAhmed151 <niaz151@gmail.com>
2019-06-06 19:15:30 -0400

    Made Logo Larger and Different Font Size

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,11 @@ class Header extends React.Component{
     return(
       <div class="w-100 header-container">
         <nav class="navbar fixed-top navbar-expand-lg navbar-light container header">
-          <a class="navbar-brand text-light" href="#"> 
+          <a href="#"> 
             <img src={require("../images/brain.png")} className="brain-img" />
-            &nbsp;  
-            SCL
           </a>
+          <span class="navbar-brand text-light header-logo header-link" style={{marginLeft:"10px"}}> SCL </span>
+          
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
