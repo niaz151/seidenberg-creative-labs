7acc7ba958544217d5242b13fb3a7096374fff0a
NiazAhmed151 <niaz151@gmail.com>
2019-06-06 19:37:33 -0400

    Added Hero Title and Subtitle

--- src/pages/Home/Hero.js
+++ src/pages/Home/Hero.js
@@ -4,9 +4,13 @@ import CurvedBottom from '../../components/CurvedBottom';
 const Hero = () => {
 
   return(
-    <div className="container-fluid d-flex align-items-center position-relative h-75 home-hero" style={{justifyContent:"center"}}>
-      <div className="container h-25">
-        Heero
+    <div className="container-fluid d-flex align-items-center justify-content-center flex-column position-relative h-75 home-hero">
+      <div className="container w-75 d-flex align-items-center justify-content-center home-hero-title-container">
+        <span className="home-hero-title"> Seidenberg </span>
+        <span className="home-hero-title home-hero-title-heavy"> Creative Labs </span>
+      </div>
+      <div className="container w-50 d-flex align-items-center justify-content-center home-hero-subtitle-container ">
+        <span className="home-hero-subtitle"> Technology Research and Development </span>
       </div>
       <CurvedBottom color="white" />
     </div>
