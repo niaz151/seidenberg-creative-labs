f289dd2b536a071e534e208dd7893dd29910169d
NiazAhmed151 <niaz151@gmail.com>
2019-06-10 17:24:57 -0400

    Our Skills Mobile Styles

--- src/pages/Home/OurSkills.js
+++ src/pages/Home/OurSkills.js
@@ -3,8 +3,52 @@ import React from 'react';
 const OurSkills = () => {
     
     return(
-      <div className="container-fluid border our-skills-container"> 
-        Our Skills
+      <div className="container-fluid d-flex flex-column align-items-center our-skills-container"> 
+        <div className="container-fluid w-50 d-flex align-items-center justify-content-center our-skills-header">
+          Our Skills
+        </div>
+        <div className="container-fluid w-75 d-flex align-items-center justify-content-center our-skills-icon-container">
+          <div className="container-fluid  d-flex align-items-center justify-content-center our-skills-icon">
+            <div className="container-fluid d-flex align-items-center justify-content-center our-skills-icon-img">
+              <i className="fa fa-cogs fa-2x icon"></i>
+            </div>
+            <div>
+              <div className="container-fluid d-flex align-items-center justify-content-center our-skills-icon-header">
+                Development
+              </div>
+              <div className="container-fluid d-flex align-items-center justify-content-center our-skills-icon-desc">
+                Working side by side with clients, we create products which bring their visions to life. Anything is possible.
+              </div>
+            </div>
+          </div>
+          <div className="container-fluid d-flex align-items-center justify-content-center our-skills-icon">
+            <div className="container-fluid d-flex align-items-center justify-content-center our-skills-icon-img">
+              <i className="fas fa-chart-bar fa-2x icon"></i>
+            </div>
+            <div className="container-fluid d-flex align-items-center justify-content-center our-skills-icon-header">
+                Research
+            </div>
+            <div className="container-fluid d-flex align-items-center justify-content-center our-skills-icon-desc">
+              Working side by side with clients, we create products which bring their visions to life. Anything is possible.
+            </div>
+          </div>
+          <div className="container-fluid d-flex align-items-center justify-content-center our-skills-icon">
+            <div className="container-fluid d-flex align-items-center justify-content-center our-skills-icon-img">
+              <i className="far fa-lightbulb fa-2x icon"></i>
+            </div>
+            <div className="container-fluid d-flex align-items-center justify-content-center our-skills-icon-header">
+                Consultancy
+            </div>
+            <div className="container-fluid d-flex align-items-center justify-content-center our-skills-icon-desc">
+              Working side by side with clients, we create products which bring their visions to life. Anything is possible.
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid w-50 d-flex align-items-center justify-content-center ours-skills-btn-container">
+          <div className="container-fluid d-flex align-items-center justify-content-center our-skills-btn">
+            Check Out Our Skills
+          </div>
+        </div>
       </div>
     )
 }
