e51afa80d75e9167befd5106f6c5ebd19553cb8d
NiazAhmed151 <niaz151@gmail.com>
2019-06-17 14:42:40 -0400

    Fixed Section Spacing for Our Skills Section

---
+++ build/precache-manifest.a478c09999db8413d93e20bcfc168e8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "137ddc62096f1a395a0927dcbbdfaa6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f43a67dbb95f0aa67c7d",
+    "url": "/static/css/main.4233a78f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "f43a67dbb95f0aa67c7d",
+    "url": "/static/js/main.514530b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a785f868845a5b398c1a2a9e02fc91be",
+    "url": "/static/media/brain.a785f868.png"
+  },
+  {
+    "revision": "37f24f94e06f76fbc8bab0bffa3284ef",
+    "url": "/static/media/laptop.37f24f94.png"
+  }
+]);
