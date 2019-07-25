3246de4b31efd3eb594be6a3877e3480d9750fcf
NiazAhmed151 <niaz151@gmail.com>
2019-07-25 13:44:29 -0400

    Added Consultancy Landscape Styling

--- src/pages/Team/TeamHeader.js
+++ src/pages/Team/TeamHeader.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const TeamHeader = () => {
   return(
-    <div>
-      
+    <div className="container-fluid d-flex flex-row align-items-center justify-content-center team-header-container">
+      <div className="container-fluid team-header-img-div">
+        <img src={require('../../images/portrait_sample.png')} className="team-header-img" />
+      </div>
+      <div className="container-fluid d-flex flex-column align-items-center justify-content-center team-header-text-div">
+        <div className="container-fluid text-center d-flex flex-column align-items-center justify-content-center team-header-top-text">
+          Committed Students
+        </div>
+        <div className="container-fluid d-flex flex-column align-items-center justify-content-center team-header-bottom-text">
+          Motivated students led by experienced seniors and alumni to make professional and impressive products. Each team 
+          adds its own unique flavor to the project, making for a constantly evolving group of serious developers.
+        </div>
+      </div>
     </div>
   )
 }
